bfd3bf1172d09e49a5d1895018e74cf0abeda0fc
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2021-10-21 12:02:29 +0400

    MVVM ShoppingList App - SHOPPING ITEM, DAO, SQLITE DATABASE - Part 2

---
+++ app/src/main/java/com/example/pl_android/ShoppingItem.kt
@@ -0,0 +1,21 @@
+package com.example.pl_android
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_item")
+data class ShoppingItem(
+
+    @ColumnInfo(name = "item_info")
+    var name: String,
+
+    @ColumnInfo(name = "item_amount")
+    var amount: Int
+
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+
+}
