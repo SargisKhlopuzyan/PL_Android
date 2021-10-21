84c79b445b7dbc22096eed764703ad77a4d9df29
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2021-10-21 18:39:57 +0400

    MVVM ShoppingList App - KODEIN DEPENDENCY INJECTION - Part 6

--- app/src/main/java/com/example/pl_android/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/pl_android/ui/shoppinglist/ShoppingActivity.kt
@@ -1,27 +1,30 @@
 package com.example.pl_android.ui.shoppinglist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pl_android.R
-import com.example.pl_android.data.db.ShoppingDatabase
 import com.example.pl_android.data.db.entities.ShoppingItem
-import com.example.pl_android.data.repositories.ShoppingRepository
 import com.example.pl_android.other.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
+//        val database = ShoppingDatabase(this)
+//        val repository = ShoppingRepository(database)
+//        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
