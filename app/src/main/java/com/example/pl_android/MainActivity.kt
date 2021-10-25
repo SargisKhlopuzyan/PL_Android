8e2e0bd33a03f6e857de2f40826ff5b21c7b0d8a
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2021-10-25 14:57:55 +0400

    PERMISSIONS - Android Fundamentals

--- app/src/main/java/com/example/pl_android/MainActivity.kt
+++ app/src/main/java/com/example/pl_android/MainActivity.kt
@@ -1,13 +1,97 @@
 package com.example.pl_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonRequestPermission.setOnClickListener {
+            requestPermissions()
+        }
     }
 
+    private fun hasWriteExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationForegroundPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationBackgroundPermission() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED)
+        } else {
+            true
+        }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (!hasWriteExternalStoragePermission()) {
+            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (!hasLocationForegroundPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+
+    }
+
+    private fun requestPermissionForAccessBackgroundLocation() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (!hasLocationBackgroundPermission() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("LOG_TAG", "${permissions[i]} was granted")
+
+                    if (permissions[i] == Manifest.permission.ACCESS_COARSE_LOCATION) {
+                        requestPermissionForAccessBackgroundLocation()
+                    }
+
+                } else {
+                    Log.e("LOG_TAG", "${permissions[i]} was denied")
+                }
+            }
+        }
+    }
 }
