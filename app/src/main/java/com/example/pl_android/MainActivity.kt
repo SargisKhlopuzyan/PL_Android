40109177453e72516d06ee6ba7b02681d664d83b
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2021-10-27 09:10:35 +0400

    Starting our First Coroutine - Kotlin Coroutines

--- app/src/main/java/com/example/pl_android/MainActivity.kt
+++ app/src/main/java/com/example/pl_android/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.pl_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch{
+            delay(6000L)
+            Log.e("LOG_TAG", "Coroutine says hello thread ${Thread.currentThread().name}")
+        }
+
+        Log.e("LOG_TAG", "Hello thread ${Thread.currentThread().name}")
     }
 }
